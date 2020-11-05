feb661eed4fd0cb0344d9df44cd5801a233f2a05
Admin <sgpsection@gmail.com>
2020-11-05 20:39:57 +0530

    FEEDBACK + CONTACT US

--- app/src/main/java/com/example/sgp/Buyer_Section/Buyer_Dashboard.java
+++ app/src/main/java/com/example/sgp/Buyer_Section/Buyer_Dashboard.java
@@ -1,19 +1,13 @@
 package com.example.sgp.Buyer_Section;
 
-import android.app.Dialog;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,7 +16,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.sgp.Adapters.Database_Class;
 import com.example.sgp.Adapters.undelivered_BuyerPending_Adapter;
+import com.example.sgp.OptionMenu.ContactUs;
 import com.example.sgp.Dashboard;
+import com.example.sgp.OptionMenu.Feedback;
 import com.example.sgp.MainActivity;
 import com.example.sgp.OptionMenu.AccountActivity;
 import com.example.sgp.R;
@@ -102,18 +98,19 @@ public class Buyer_Dashboard extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.seller_menu_item:
-                Toast.makeText(this, "Seller Selected", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(this, Seller_Dashboard.class));
                 break;
             case R.id.buyer_menu_item:
-                Toast.makeText(this, "Buyer Selected", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(this, Buyer_Dashboard.class));
-
                 break;
             case R.id.home_menu_item:
-                Toast.makeText(this, "Home Selected", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(this, Dashboard.class));
-
+                break;
+            case R.id.ContactUs_menu_item:
+                startActivity(new Intent(this, ContactUs.class));
+                break;
+            case R.id.feedback_menu_item:
+                startActivity(new Intent(this, Feedback.class));
                 break;
             case R.id.account_menu_item:
                 Intent intent1 = new Intent(this, AccountActivity.class);
