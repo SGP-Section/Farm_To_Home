b8121f3b99c5689b9e4ceacc5c6293fc03a86690
SGP-Section <sgpsection@gmail.com>
2020-11-06 09:54:25 +0530

    Final 06-11-2020

--- app/src/main/java/com/example/sgp/OptionMenu/ContactUs.java
+++ app/src/main/java/com/example/sgp/OptionMenu/ContactUs.java
@@ -1,9 +1,13 @@
 package com.example.sgp.OptionMenu;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.example.sgp.Buyer_Section.CallActivity;
 import com.example.sgp.R;
 
 public class ContactUs extends AppCompatActivity {
@@ -12,5 +16,24 @@ public class ContactUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+        ImageButton dhruv,charmi;
+        charmi=findViewById(R.id.call_Charmi);
+        dhruv=findViewById(R.id.call_Dhruv);
+        charmi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ContactUs.this, CallActivity.class);
+                intent.putExtra("PhNoValue","+919327286582");
+                ContactUs.this.startActivity(intent);
+            }
+        });
+        dhruv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ContactUs.this, CallActivity.class);
+                intent.putExtra("PhNoValue", "+919726434049");
+                ContactUs.this.startActivity(intent);
+            }
+        });
     }
 }
