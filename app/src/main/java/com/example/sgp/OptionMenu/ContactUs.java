7f017aa07aeb0bef0e821ef44747a8757e53311d
SGP-Section <sgpsection@gmail.com>
2020-11-08 11:08:18 +0530

    Commit 08-11-2020

--- app/src/main/java/com/example/sgp/OptionMenu/ContactUs.java
+++ app/src/main/java/com/example/sgp/OptionMenu/ContactUs.java
@@ -1,33 +1,39 @@
 package com.example.sgp.OptionMenu;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sgp.Buyer_Section.CallActivity;
+import com.example.sgp.Dashboard;
 import com.example.sgp.R;
 
 public class ContactUs extends AppCompatActivity {
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+        startActivity(new Intent(this, Dashboard.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
-        ImageButton dhruv,charmi;
-        charmi=findViewById(R.id.call_Charmi);
-        dhruv=findViewById(R.id.call_Dhruv);
-        charmi.setOnClickListener(new View.OnClickListener() {
+
+        findViewById(R.id.call_Charmi).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(ContactUs.this, CallActivity.class);
-                intent.putExtra("PhNoValue","+919327286582");
+                intent.putExtra("PhNoValue", "+919327286582");
                 ContactUs.this.startActivity(intent);
             }
         });
-        dhruv.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.call_Dhruv).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(ContactUs.this, CallActivity.class);
@@ -35,5 +41,25 @@ public class ContactUs extends AppCompatActivity {
                 ContactUs.this.startActivity(intent);
             }
         });
+        findViewById(R.id.email_Dhruv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendEmail("dkpatel2491@gmail.com");
+
+            }
+        });
+        findViewById(R.id.email_charmi).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendEmail("charmi261101@gmail.com");
+            }
+        });
+    }
+
+    protected void sendEmail(String Mail) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("mailto:" + Mail + "?subject=" + "&body="));
+        startActivity(intent);
+
     }
 }
