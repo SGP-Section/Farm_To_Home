a7acd2091816a4f7c9bde035648c4b7ca2268e06
SGP-Section <sgpsection@gmail.com>
2021-04-29 03:10:41 +0530

    Commit 29-04-2021 Corrections 1.0

--- app/src/main/java/com/example/sgp/MainActivity.java
+++ app/src/main/java/com/example/sgp/MainActivity.java
@@ -25,54 +25,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
-finish();
-            startActivity(new Intent(MainActivity.this, Login_Page.class));
-        }
+
         final ProgressBar spin = (ProgressBar) findViewById(R.id.progressBar_intro);
+        spin.setVisibility(View.VISIBLE);
+        new CountDownTimer(1000, 500) {
+            @Override
+            public void onTick(long l) {
+            }
 
-        //Initialize connectivityManager
-        ConnectivityManager connectivityManager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+            @Override
+            public void onFinish() {
+                ConnectivityManager connectivityManager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+                if (networkInfo == null || !networkInfo.isConnected() || !networkInfo.isAvailable()) {
+                    //when internet is inactive
 
-        //Get active network Information
-        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-
-
-
-        if (networkInfo == null || !networkInfo.isConnected() || !networkInfo.isAvailable()) {
-            //when internet is inactive
-
-            Dialog dialog = new Dialog(this);
-            dialog.setContentView(R.layout.network_alert_dialog);
-            dialog.setCanceledOnTouchOutside(false);
-            dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
-            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
-            dialog.getWindow().getAttributes().windowAnimations = android.R.style.Animation_Dialog;
-            Button btnTryAgainNetwork = dialog.findViewById(R.id.btn_tryAgain_network);
-            btnTryAgainNetwork.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    recreate();
-                }
-            });
-            dialog.show();
-        } else {
-
-            //when internet is Active
-            spin.setVisibility(View.VISIBLE);
-            new CountDownTimer(1000, 500) {
-                @Override
-                public void onTick(long l) {
-                }
-
-                @Override
-                public void onFinish() {
+                    Dialog dialog = new Dialog(MainActivity.this);
+                    dialog.setContentView(R.layout.network_alert_dialog);
+                    dialog.setCanceledOnTouchOutside(false);
+                    dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+                    dialog.getWindow().getAttributes().windowAnimations = android.R.style.Animation_Dialog;
+                    Button btnTryAgainNetwork = dialog.findViewById(R.id.btn_tryAgain_network);
+                    btnTryAgainNetwork.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            recreate();
+                        }
+                    });
+                    dialog.show();
+                } else if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+                    finish();
+                    startActivity(new Intent(MainActivity.this, Login_Page.class));
+                } else {
                     startActivity(new Intent(MainActivity.this, Login_Page.class));
                     finish();
-                }
-            }.start();
 
-        }
+                }
+
+
+            }
+        }.start();
+
 
     }
 }
